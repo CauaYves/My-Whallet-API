47e5ed783bd8f24250a485420e65100f231272b0
Yves <cauayves5@gmail.com>
2023-04-21 18:21:02 -0300

    cors configured for all ports

--- src/index.js
+++ src/index.js
@@ -5,9 +5,12 @@ import router from "./routes/index.routes.js"
 const app = express()
 app.use(express.json())
 app.use(router)
-app.use(cors({
-    origin: 'http://localhost:3000',
-    optionsSuccessStatus: 200 
-  }));
+
+app.use((req,res,next) => {
+  req.header("Acess-Control-Allow-Origin", "*")
+  app.use(cors())
+  next()
+})
+
 const port = process.env.PORT || 4000
 app.listen(port, console.log(`server running on port ${port}`))
