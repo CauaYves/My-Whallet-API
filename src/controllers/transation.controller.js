94713d0864120296a1477168b1b6751f730f95d5
Yves <cauayves5@gmail.com>
2023-04-22 14:10:37 -0300

    feat: transation whallet

--- src/controllers/transation.controller.js
+++ src/controllers/transation.controller.js
@@ -1,11 +1,13 @@
 import dayjs from "dayjs"
 import { db } from "../database/database.connection.js"
 import { entrySchema } from "../schema/user.schema.js"
+import { ObjectId } from "mongodb"
 
 export async function transacao(req, res) {
 
     const { type, value, description } = req.body
     const { authorization } = req.headers
+    
 
     if (!authorization) return res.sendStatus(401)
 
@@ -15,12 +17,13 @@ export async function transacao(req, res) {
     const token = authorization?.replace("Bearer ", "")
 
     const tokenStoked = await db.collection("sessoes").findOne({ token: token })
+    const userId = new ObjectId(tokenStoked.userid)
     const user = await db.collection("cadastros").findOne({ _id: tokenStoked.userid })
 
     delete user.password
     delete user.email
     delete user.name
-    console.log(user)
+    
     await db.collection("transacoes").insertOne({
         user,
         type,
@@ -28,6 +31,8 @@ export async function transacao(req, res) {
         description,
         date: dayjs().format('DD/MM')
     })
-
-    res.sendStatus(201)
+    const transacoes = await db.collection("transacoes").find().toArray()
+    //filtrar transacoes e enviar apenas as do usuario correspondente ao token
+    const userTransactions = transacoes.filter(transacao => transacao.user._id.equals(userId));
+    res.status(200).send(userTransactions)
 }
--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -2,11 +2,11 @@ import { Router } from "express";
 import { cadastro, login } from "../controllers/auth.controller.js";
 import { transacao } from "../controllers/transation.controller.js";
 import validateSchema from "../middlewares/validateSchema.middleware.js";
-import { loginSchema } from "../schema/user.schema.js";
+import { loginSchema, registerSchema } from "../schema/user.schema.js";
 
 const router = Router()
 
-router.post("/cadastro", validateSchema(loginSchema) ,cadastro)
+router.post("/cadastro", validateSchema(registerSchema) ,cadastro)
 router.post("/login", login)
 router.post("/nova-transacao/:tipo", transacao)
 
