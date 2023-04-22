635fc68370f68db226c329e853644f8e26092cee
Yves <cauayves5@gmail.com>
2023-04-21 22:50:01 -0300

    feat: route transation

--- src/controllers/transation.controller.js
+++ src/controllers/transation.controller.js
@@ -1,12 +1,27 @@
-
+import { db } from "../database/database.connection.js"
+import { entrySchema } from "../schema/user.schema.js"
 
 export async function transacao(req, res) {
 
-    const { tipo } = req.params
-    const { token } = req.body
+    const { type, value, description } = req.body
+    const { authorization } = req.headers
 
-    if (!token) return res.sendStatus(401)
-    if (tipo % 1 !== 0) res.sendStatus(422)
+    if (!authorization) return res.sendStatus(401)
 
-    res.sendStatus(200)
+    const { error } = entrySchema.validate(req.body)
+    if (error) return res.status(422).send("dados inválidos, certifique de que todos os campos estão preenchidos e no formato correto")
+
+    const token = authorization?.replace("Bearer ", "")
+
+    const tokenStoked = await db.collection("sessoes").findOne({ token: token })
+    const user = await db.collection("cadastros").findOne({ _id: tokenStoked.userid })
+
+    await db.collection("transacoes").insertOne({
+        user,
+        type,
+        value,
+        description
+    })
+
+    res.sendStatus(201)
 }
--- src/schema/user.schema.js
+++ src/schema/user.schema.js
@@ -10,3 +10,9 @@ export const loginSchema = joi.object({
     email: joi.string().email(),
     password: joi.string().min(3)
 })
+
+export const entrySchema = joi.object({
+    value: joi.number().positive().precision(2).strict().required(),
+    type: joi.string().required(),
+    description: joi.string().required()
+})
